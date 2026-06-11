d0a01ba1dfdeca68b062f86c60e0e170c1c98d60
Henrique Dell <henriquedell1@gmail.com>
2026-06-11 17:04:02 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,15 @@
 enum Command {
   CMD_UNKNOWN,
   CMD_EXIT,
-  CMD_ECHO
+  CMD_ECHO,
+  CMD_TYPE
 };
 
 Command resolveCommand(const std::string& input) {
   static const std::map<std::string, Command> commandMap = {
     {"exit", CMD_EXIT},
-    {"echo", CMD_ECHO}
+    {"echo", CMD_ECHO},
+    {"type", CMD_TYPE}
   };
   
   auto it = commandMap.find(input);
@@ -20,6 +22,25 @@ Command resolveCommand(const std::string& input) {
   }
 
   return CMD_UNKNOWN;
+}
+
+void evalBuiltIn(const std::string& command, const std::string& args) {
+  std::string cmd = command + " " + args;
+
+  if (resolveCommand(args) == CMD_UNKNOWN) {
+    std::cout << args << ": command not found" << std::endl;
+    return;
+  }
+
+  FILE* pipe = popen(cmd.c_str(), "r");
+
+  if (pipe) {
+    char buffer[128];
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+      std::cout << buffer;
+    }
+    pclose(pipe);
+  }
 }
 
 int main() {
@@ -33,6 +54,7 @@ int main() {
     std::getline(std::cin, fullCommand);
 
     std::string command = fullCommand.substr(0, fullCommand.find(' '));
+    std::string args = fullCommand.substr(fullCommand.find(' ') + 1);
 
     switch (resolveCommand(command)) {
       case CMD_EXIT:
@@ -40,6 +62,9 @@ int main() {
         break;
       case CMD_ECHO:
         std::cout << fullCommand.substr(fullCommand.find(' ') + 1) << std::endl;
+        break;
+      case CMD_TYPE:
+        evalBuiltIn(command, args);
         break;
       default:
         std::cerr << command << ": command not found" << std::endl;
