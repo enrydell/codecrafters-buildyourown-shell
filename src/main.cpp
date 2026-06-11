bc71c9a0cf28d9f76123f074ab343fb11ccfc439
Henrique Dell <henriquedell1@gmail.com>
2026-06-11 16:17:19 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-
-  std::string command;
-  std::getline(std::cin, command);
-  // if (command == "echo hello world") {
-  //   std::cout << "hello world" << std::endl;
-  // } else {
-    std::string first_word = command.substr(0, command.find(' '));
-    std::cerr << first_word << ": command not found" << std::endl;
-  // }
+  while(true) {
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
+    // if (command == "echo hello world") {
+    //   std::cout << "hello world" << std::endl;
+    // } else {
+      std::string first_word = command.substr(0, command.find(' '));
+      std::cerr << first_word << ": command not found" << std::endl;
+    // }
+  }
 }
